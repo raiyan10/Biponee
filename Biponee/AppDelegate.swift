2b108097cee6f57f5756ae0d3e46963499665e17
Raiyan Yousuf <raiyan.austcse10@gmail.com>
2015-05-12 21:30:35 +0600

    Login with FB

--- Biponee/AppDelegate.swift
+++ Biponee/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,12 +17,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        
         // Override point for customization after application launch.
         let pageControl: UIPageControl = UIPageControl.appearance()
         pageControl.pageIndicatorTintColor = UIColor.lightGrayColor()
         pageControl.currentPageIndicatorTintColor = UIColor.orangeColor()
         pageControl.backgroundColor = UIColor.whiteColor()
-        return true
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+    
+    func application(application: UIApplication, openURL url: NSURL, sourceApplication: String?, annotation: AnyObject?) -> Bool {
+        
+        return FBSDKApplicationDelegate.sharedInstance().application(application, openURL: url, sourceApplication: sourceApplication, annotation: annotation)
+
     }
 
     func applicationWillResignActive(application: UIApplication) {
@@ -39,6 +49,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        FBSDKAppEvents.activateApp();
     }
 
     func applicationWillTerminate(application: UIApplication) {
--- Biponee/LoginViewController.swift
+++ Biponee/LoginViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class LoginViewController: UIViewController {
 
@@ -33,7 +35,7 @@ class LoginViewController: UIViewController {
         }
         
         self.containerView.layer.cornerRadius = 8.0
-        self.fbLoginButton.layer.cornerRadius = 4.0
+        self.fbLoginButton.layer.cornerRadius = 8.0
         self.loginButton.layer.cornerRadius = 4.0
         
         self.createAccButton.layer.cornerRadius = 4.0
@@ -62,12 +64,66 @@ class LoginViewController: UIViewController {
         
     }
     
-    @IBAction func rememberEmail(sender: AnyObject)
+    @IBAction func loginWithFB(sender: AnyObject)
     {
+        if ((FBSDKAccessToken.currentAccessToken()) != nil)
+        {
+            // User is logged in, do work such as go to next view controller.
+            println("Access Token Saved: \(FBSDKAccessToken.currentAccessToken().tokenString)")
+            return
+        }
         
+        var login: FBSDKLoginManager = FBSDKLoginManager()
+        login.logInWithReadPermissions(["public_profile", "email"], handler: { (result, error) -> Void in
+            
+            println("User Logged In")
+            
+            if (error != nil)
+            {
+                // Process error
+                println("Error: \(error)")
+            }
+            else if result.isCancelled
+            {
+                // Handle cancellations
+            }
+            else
+            {
+                // If you ask for multiple permissions at once, you should check if specific permissions missing
+                if result.grantedPermissions.contains("email")
+                {
+                    // Do work
+                }
+                
+                self.returnUserData()
+            }
+        })
     }
     
-    @IBAction func loginWithFB(sender: AnyObject)
+    func returnUserData()
+    {
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath: "me", parameters: nil)
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+            if (error != nil)
+            {
+                // Process error
+                println("Error: \(error)")
+            }
+            else
+            {
+                println("Access Token: \(FBSDKAccessToken.currentAccessToken().tokenString)")
+                
+                println("Fetched User: \(result)")
+                let userName : NSString = result.valueForKey("name") as! NSString
+                println("User Name is: \(userName)")
+                let userEmail : NSString = result.valueForKey("email") as! NSString
+                println("User Email is: \(userEmail)")
+            }
+        })
+    }
+    
+    @IBAction func rememberEmail(sender: AnyObject)
     {
         
     }
